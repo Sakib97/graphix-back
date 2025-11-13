4879e46e97ba07a9a546eea41047e146b76e998a
Sakib97 <ashekseum86@gmail.com>
2025-11-13 20:58:56 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://127.0.0.1:5173", 
+           "http://localhost:5173",
+           "https://graphix-rho.vercel.app/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # Specify the frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"Message": "Welcome to graphix backend!"}
+
+
+# command for 
+    #  installing all from requirements.txt: 
+            #  pip install -r requirements.txt
+    # settign up venv:  python -m venv venv
+    # activate venv: .\venv\Scripts\activate
+    # deactivate venv: deactivate
+    # see all inatalled libraries: pip list"
+    # run uvicorn server: uvicorn main:app --reload --port 8080
+# python -u "d:\graphix-back\graphix-back\core\tempCodeRunnerFile.py"
